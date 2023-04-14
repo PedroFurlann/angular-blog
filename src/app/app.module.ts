2604e4f4cc29a4bb3333d9933cbaccd1479f6339
Pedro Furlan <pedrofurlan6@gmail.com>
2023-04-14 10:28:50 -0300

    Create components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
