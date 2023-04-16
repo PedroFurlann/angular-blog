ea1cd814bd692508f7ba868023feb1248508ae52
Pedro Furlan <pedrofurlan6@gmail.com>
2023-04-16 15:56:52 -0300

    Update content component

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,15 @@
 export const dataFake = [
   {
-    "id": 1,
-    "img": 'aaa',
-    "title": 'saiu novo filme',
-    "description": 'ola',
+    "id": "1",
+    "img": 'https://cineover.files.wordpress.com/2013/02/homemdeferro3_17.jpg',
+    "title": 'Novo Homem de ferro',
+    "description": 'Marvel anuncia novo homem de ferro',
   },
 
   {
-    "id": 2,
-    "img": 'aaa',
-    "title": 'saiu novo filme',
-    "description": 'ola',
+    "id": "2",
+    "img": 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6DAA31D2EAD43853A24CCBD79A8C38E0EF299DEBEF59CC4100544C65AACC252D/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    "title": 'Nova série WandaVision anunciada no Disney Plus',
+    "description": 'Disney anuncia nova sério WandaVision',
   },
 ];
