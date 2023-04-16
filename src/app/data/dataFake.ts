9e9ab002461222dfddf889c75f92eff598d11ac6
Pedro Furlan <pedrofurlan6@gmail.com>
2023-04-16 20:32:06 -0300

    Update the series and movies on content page

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,6 +10,19 @@ export const dataFake = [
     "id": "2",
     "img": 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6DAA31D2EAD43853A24CCBD79A8C38E0EF299DEBEF59CC4100544C65AACC252D/scale?width=1200&aspectRatio=1.78&format=jpeg',
     "title": 'Nova série WandaVision anunciada no Disney Plus',
-    "description": 'Disney anuncia nova sério WandaVision',
+    "description": 'Disney anuncia nova série WandaVision',
   },
+  {
+    "id": "3",
+    "img": 'https://pbs.twimg.com/media/E23__5wXEAMMZTI.jpg',
+    "title": "Nova série Loki anunciada no Disney Plus",
+    "description": "Disney anuncia nova série Loki"
+  },
+  {
+    "id": "4",
+    "img": 'https://disneyplusbrasil.com.br/wp-content/uploads/2022/10/Pantera-Negra-2-Shuri.jpg',
+    "title": "Nova filme do pantera negra é anunciado",
+    "description": "Filme do pantera negra anunciado recentemente",
+
+  }
 ];
