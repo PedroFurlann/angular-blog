2857d7e6996e52bab68909cfaf2b27a06360fd09
Pedro Furlan <pedrofurlan6@gmail.com>
2023-04-16 15:12:59 -0300

    Getting informations by route id

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  imgCover: string = ''
-  contentTitle: string = ''
+  imgCover: string = '';
+  contentTitle: string = '';
   contentDescription: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => console.log(value.get('id')));
   }
-
 }
